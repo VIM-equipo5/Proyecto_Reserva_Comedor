c90bb1ca16450d1e0bc52ab224b54a34a4c925ef
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-17 15:40:41 +0100

    Delete ReservaComedor directory

--- ReservaComedor/src/main/java/com/example/demo/dao/ICategoriaDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.dto.Categoria;
-
-public interface ICategoriaDAO extends JpaRepository<Categoria, Integer> {
-
-}
--- ReservaComedor/src/main/java/com/example/demo/dao/IPlatosDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.dto.Platos;
-
-public interface IPlatosDAO extends JpaRepository<Platos, Integer> {
-
-}
