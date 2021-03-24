9408e0d392e81ecb850dbd5a6f79678cb85516b2
Miguel <sastre113@gmail.com>
2021-03-24 09:28:44 +0100

    Cambios en la estructura. Diseño del login y registro terminado.

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home/home.component';
 import { LoginComponent } from './Login/login/login.component';
+import { RegisterComponent } from './Login/register/register.component';
 
 const routes: Routes = [
-  {path:'', pathMatch:'full',redirectTo:'home' },
+  {path:'', pathMatch:'full',redirectTo:'login' },
   {path:'home',component:HomeComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'logup',component:RegisterComponent}
 ];
 
 @NgModule({
