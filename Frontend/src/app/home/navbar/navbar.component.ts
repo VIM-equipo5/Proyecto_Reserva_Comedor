e8d97dc3e90c7bf6b3dc641fd3f677134d145902
Miguel <sastre113@gmail.com>
2021-03-25 20:07:55 +0100

    Añadida barra de navegación

--- Frontend/src/app/home/navbar/navbar.component.ts
+++ Frontend/src/app/home/navbar/navbar.component.ts
@@ -7,28 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent {
 
-  menuActivo = false;
-  id = "layoutSidenav";
-
   constructor() {}
 
   ngOnInit(): void {}
 
-  desplegarMenuLateral(): void {
-    console.log("hola")
-
-    let boton = document.getElementById(this.id);
-    if(!this.menuActivo){
-      this.id = "layoutSidenav active";
-      boton?.setAttribute("id", this.id);
-      this.menuActivo = true;
-    } else {
-      this.id = "layoutSidenav";
-      boton?.setAttribute("id", this.id);
-      this.menuActivo = false;
-    }
-      
-    
-    
-  }
 }
