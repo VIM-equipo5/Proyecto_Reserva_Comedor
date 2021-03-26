8890aea130fbd45f110231bba545280a55b7570d
VyacheslavKhaydorov <78428134+VyacheslavKhaydorov@users.noreply.github.com>
2021-03-26 17:22:34 +0100

    Los botones del desplegable de home ya no desaparecen

--- Frontend/src/app/home/modal/reservas-usuario/reservas-usuario.component.ts
+++ Frontend/src/app/home/modal/reservas-usuario/reservas-usuario.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-reservas-usuario',
   templateUrl: './reservas-usuario.component.html',
-  styleUrls: ['./reservas-usuario.component.css']
+  styleUrls: ['./reservas-usuario.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  styles: [
+    `
+      .backdrop {
+        background-color: #5cb3fd;
+      }
+    `,
+  ],
 })
-export class ReservasUsuarioComponent implements OnInit {
+export class ReservasUsuarioComponent{
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit(): void {
+  openXl(content: any) {
+    this.modalService.open(content, {
+      backdropClass: "backdrop",
+      size: "xl",
+      centered: true,
+      scrollable: true
+    });
   }
 
 }
