a85a9bf9903f571e0662dff8d40fe8c2de61ddf3
VyacheslavKhaydorov <78428134+VyacheslavKhaydorov@users.noreply.github.com>
2021-03-26 16:20:37 +0100

    Modales en proceso y algunos cambios de estilo (navbar fixed-top, home y admin css), nuevos componentes

---
+++ Frontend/src/app/home/modal/reservas-usuario/reservas-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservas-usuario',
+  templateUrl: './reservas-usuario.component.html',
+  styleUrls: ['./reservas-usuario.component.css']
+})
+export class ReservasUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
